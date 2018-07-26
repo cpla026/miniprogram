a69570dc9adde5863aad5207822459b818464e39
cpla026 <xf17671757026@163.c0m>
2018-07-27 00:08:25 +0800

    init miniprogram

--- miniprogram_first/src/App.vue
+++ miniprogram_first/src/App.vue
@@ -6,27 +6,7 @@
   // import qcloud from 'wafer2-client-sdk'
   export default {
     // async 和 await 一起使用
-    async created () {
-      // 获取用户信息
-      wx.getSetting({
-        success: res => {
-          if (res.authSetting['scope.userInfo']) {
-            // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-            wx.getUserInfo({
-              success: res => {
-                // 可以将 res 发送给后台解码出 unionId
-                this.userInfo = res.userInfo
-
-                // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-                // 所以此处加入 callback 以防止这种情况
-                if (this.userInfoReadyCallback) {
-                  this.userInfoReadyCallback(res)
-                }
-              }
-            })
-          }
-        }
-      })
+    created () {
       // 获取用户信息
       // await 等待
       // const res = await get('/user/getUserList')
--- miniprogram_first/src/pages/me/Me.vue
+++ miniprogram_first/src/pages/me/Me.vue
@@ -1,33 +1,72 @@
 <template>
-  <view class="userinfo">
-    <button v-if="!hasUserInfo && canIUse" open-type="getUserInfo" :click="getUserInfo"> 获取头像昵称 </button>
-    <view v-else>
-      <img :click="bindViewTap" class="userinfo-avatar" src="{{userInfo.avatarUrl}}" mode="cover">
-      <p class="userinfo-nickname">{{userInfo.nickName}}</p>
-    </view>
-  </view>
-  <view class="usermotto">
-    <span class="user-motto">{{motto}}</span>
-  </view>
+  <div class="userinfo" @click='login'>
+    <img :src="userinfo.avatarUrl" alt="">
+    <p>{{userinfo.nickName}}</p>
+  </div>
 </template>
 <script>
-export default {
-  data: {
-    motto: 'Hello World',
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  create () {
-    console.log()
-  },
-  methods: {
-    getUserInfo: (e) => {
-      console.log(e)
-      this.userInfo = e.detail.userInfo
-      this.hasUserInfo = true
+  import config from '@/utils/config'
+  export default {
+    data () {
+      return {
+        userinfo: {
+          avatarUrl: '../../../static/img/unlogin.png',
+          nickName: '点击登录'
+        }
+      }
+    },
+    methods: {
+      login () {
+        let user = wx.getStorageSync('userinfo')
+        const self = this
+        if (!user) {
+          wx.login({
+            success: (res) => {
+              if (res.code) {
+              // 发起网络请求
+                wx.request({
+                  url: config.host + '/user/getOpenId',
+                  data: {
+                    code: res.code
+                  },
+                  success: (e) => {
+                    console.log(111, e)
+                    console.log(111, e.data.data.openid)
+                    if (e.data.data.openid) {
+                      wx.getSetting({
+                        success (res) {
+                          if (!res.authSetting['scope.userInfo']) {
+                            wx.authorize({
+                              scope: 'scope.userInfo',
+                              success () {
+                                wx.getUserInfo({
+                                  success: function (x) {
+                                    console.log(222, x.userInfo)
+                                    wx.setStorageSync('userinfo', x.userInfo)
+                                    self.userinfo = x.userInfo
+                                    console.log(333, self.userinfo)
+                                  },
+                                  fail: function (y) {
+                                    console.log(y)
+                                  }
+                                })
+                              }
+                            })
+                          }
+                        }
+                      })
+                    }
+                  }
+                })
+              } else {
+                console.log('登录失败！' + res.errMsg)
+              }
+            }
+          })
+        }
+      }
     }
   }
-}
 </script>
 
 <style >
